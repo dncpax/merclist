6282c563fd51f020a85d2885496b1f6eec0adfc9
dncpax <dncarreira@gmail.com>
2026-09-16 16:30:08 +0100

    pwa

---
+++ sw.js
@@ -0,0 +1,34 @@
+const cacheName = "merclist-v1";
+const appShell = [
+  "./",
+  "./merclist.html",
+  "./manifest.webmanifest",
+  "./icons/icon.svg",
+  "./icons/icon-192.svg",
+  "./icons/icon-512.svg"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(cacheName).then(cache => cache.addAll(appShell)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(key => key !== cacheName).map(key => caches.delete(key))
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+      const copy = response.clone();
+      caches.open(cacheName).then(cache => cache.put(event.request, copy));
+      return response;
+    }))
+  );
+});
